cd232899b4d63006166d9a5d11bb79db6f1d72cd
Rohan Asmat <m.rohan.a.asmat@gmail.com>
2016-04-05 11:54:49 +0200

    Erros corrected on LandingPage + Color Palette

--- frontend/src/components/FooterLanding/FooterLanding.js
+++ frontend/src/components/FooterLanding/FooterLanding.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import classes from './FooterLanding.scss'
-import Row from 'muicss/lib/react/row'
-import Col from 'muicss/lib/react/col'
+import Grid from 'react-bootstrap/lib/Grid'
+import Row from 'react-bootstrap/lib/Row'
+import Col from 'react-bootstrap/lib/Col'
+
 
 type Props = {
 
@@ -12,33 +14,9 @@ export class FooterLanding extends React.Component {
   render () {
     return (
       <div>
-        <div className='row'>
-          <Row>
-          <Col md="1">md-1</Col>
-          <Col md="1">md-1</Col>
-          <Col md="1">md-1</Col>
-          <Col md="1">md-1</Col>
-          <Col md="1">md-1</Col>
-          <Col md="1">md-1</Col>
-          <Col md="1">md-1</Col>
-          <Col md="1">md-1</Col>
-          <Col md="1">md-1</Col>
-          <Col md="1">md-1</Col>
-          <Col md="1">md-1</Col>
-          <Col md="1">md-1</Col>
-        </Row>
-          <div className='col-md-3'>
-            rohan
-          </div>
-          <div className='col-md-3'>
-            rohan
-          </div>
-          <div className='col-md-6'>
-            <div className={classes.rightContent}>
-              <a> dsadsa </a>
-            </div>
-          </div>
-        </div>
+        <Grid>
+            
+        </Grid>
       </div>
     )
   }
