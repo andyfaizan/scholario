618ba30fdf3b2c1a3a5271f73109e68cd5831bb0
Ainuddin Faizan <andy@scholario.de>
2016-04-29 02:50:45 +0200

    Added containers for materials. Added folder list

--- frontend/src/views/MaterialView/MaterialView.js
+++ frontend/src/views/MaterialView/MaterialView.js
@@ -5,6 +5,8 @@ import InfoBox from '../../containers/InfoBox'
 import Grid from 'react-bootstrap/lib/Grid'
 import Row from 'react-bootstrap/lib/Row'
 import Col from 'react-bootstrap/lib/Col'
+import MaterialPanel from '../../components/MaterialPanel/MaterialPanel'
+import MaterialsList from '../../containers/MaterialsList'
 
 type Props = {
 
@@ -18,19 +20,32 @@ export class Material extends React.Component {
       <div>
         <DashboardToolBar />
         <CourseInformation />
-          <Grid fluid={true}>
-            <Row>
-              <Col md={2}>
-                <InfoBox width={200} height={100} />
-              </Col>
-              <Col md={2}>
-                <InfoBox width={200} height={100} />
-              </Col>
-              <Col md={8}>
-                <InfoBox width={800} height={100} />
-              </Col>
-            </Row>
-          </Grid>
+        <Grid fluid={true}>
+          <Row>
+            <Col md={2}>
+              <InfoBox width={200} height={100} />
+            </Col>
+            <Col md={2}>
+              <InfoBox width={200} height={100} />
+            </Col>
+            <Col md={8}>
+              <InfoBox width={800} height={100} />
+            </Col>
+          </Row>
+          <Row>
+            <Col md={4}>
+              <MaterialPanel>
+                <MaterialsList />
+              </MaterialPanel>
+            </Col>
+            <Col md={4}>
+              <MaterialPanel />
+            </Col>
+            <Col md={4}>
+              <MaterialPanel />
+            </Col>
+          </Row>
+        </Grid>
       </div>
     )
   }
---
+++ frontend/tests/components/MaterialPanel.spec.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import MaterialPanel from 'components/MaterialPanel/MaterialPanel'
+
+describe('(Component) MaterialPanel', () => {
+  it('should exist', () => {
+
+  })
+})
