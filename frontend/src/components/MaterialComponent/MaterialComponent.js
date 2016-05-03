f7cc133dd2b1e00cb359537e268705e2cfe92c74
Rohan Asmat <m.rohan.a.asmat@gmail.com>
2016-05-03 02:45:54 +0200

    skeleton materialComp

---
+++ frontend/src/components/MaterialComponent/MaterialComponent.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+type Props = {
+
+};
+export class MaterialComponent extends React.Component {
+  props: Props;
+
+  render () {
+    return (
+      <div></div>
+    )
+  }
+}
+
+export default MaterialComponent
+
---
+++ frontend/tests/components/MaterialComponent.spec.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import MaterialComponent from 'components/MaterialComponent/MaterialComponent'
+
+describe('(Component) MaterialComponent', () => {
+  it('should exist', () => {
+
+  })
+})
