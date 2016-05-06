5f03c8d5e2e55050dcf7b3ed2e24a2f2f7f0e969
Rohan Asmat <m.rohan.a.asmat@gmail.com>
2016-05-06 09:42:30 +0200

    add Course Card Improved

--- frontend/src/components/AddCourseComponent/AddCourseComponent.js
+++ frontend/src/components/AddCourseComponent/AddCourseComponent.js
@@ -15,14 +15,16 @@ export class AddCourseComponent extends React.Component {
   render () {
 
   	const addCourseCompStyle = {
-	    float: 'left',
+	  
+	  float: 'left',
 	  height: 170,
 	  width: 300,
 	  margin: 8.5,
 	  backgroundColor: '#26A65B',
 	  color: 'white',
 	  borderRadius: 13,
-	  overflow: 'inherit' 
+	  overflow: 'inherit',
+	  alignItems: 'center'
 	};
 
 	const buttonStyle = {
@@ -30,16 +32,17 @@ export class AddCourseComponent extends React.Component {
 	  margin:'auto',
 	  width:'100%',
 	  padding:10,
-	  height: 200,
+	  height: 170,
 	  lineHeight: 200
 	};
 
 	const plusButton ={
 	  width: 70,
-	  height: 70
+	  height: 70,
+	  opacity: 0.9
 	};
 
-    const actions = <div><IconButton style={buttonStyle} tooltip="Add Course"> <NoteAdd style={plusButton} /></IconButton></div> ;
+    const actions = <div><IconButton style={buttonStyle} tooltip="Add Course"> <NoteAdd style={plusButton} color="white" /></IconButton></div> ;
 
 
     return (
