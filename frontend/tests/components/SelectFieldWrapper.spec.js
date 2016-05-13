16bfcb5e8f579abf1ef55cb665abc4a529fae1ab
Sina Mahmoodi <itz.s1na@gmail.com>
2016-05-13 14:08:31 +0200

    Add SelectFieldWrapper

---
+++ frontend/tests/components/SelectFieldWrapper.spec.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import SelectFieldWrapper from 'components/SelectFieldWrapper/SelectFieldWrapper'
+
+describe('(Component) SelectFieldWrapper', () => {
+  it('should exist', () => {
+
+  })
+})
