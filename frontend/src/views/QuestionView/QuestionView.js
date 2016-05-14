3b0882bfd63374dd39bf7b579456384dd9adf5b2
Rohan Asmat <m.rohan.a.asmat@gmail.com>
2016-05-14 16:56:59 +0200

    Question View Added

---
+++ frontend/src/views/QuestionView/QuestionView.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+type Props = {
+
+};
+export class Question extends React.Component {
+  props: Props;
+
+  render () {
+    return (
+      <div></div>
+    )
+  }
+}
+
+export default Question
