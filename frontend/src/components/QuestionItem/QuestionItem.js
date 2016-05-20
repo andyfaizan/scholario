41e2a443e04666e0da20b6caab03ac5bbcd094d4
Rohan Asmat <m.rohan.a.asmat@gmail.com>
2016-05-20 16:54:05 +0200

    Sina Jhon Snow Welcomes you In House Of Starks ( QUESTIONS )

--- frontend/src/components/QuestionItem/QuestionItem.js
+++ frontend/src/components/QuestionItem/QuestionItem.js
@@ -6,6 +6,7 @@ import Avatar from 'material-ui/lib/avatar'
 import IconButton from 'material-ui/lib/icon-button'
 import ThumbsUp from 'material-ui/lib/svg-icons/action/thumb-up'
 import classes from './QuestionItem.scss'
+import { Router, Route, Link } from 'react-router'
 
 type Props = {
 
@@ -36,7 +37,13 @@ export class QuestionItem extends React.Component {
       color:'#26A65B'
  
            };
-    
+
+      const touchQuestion = () => { 
+
+              
+
+      } ;
+
     const secondaryText = <div className={styleSecondaryText}>{this.props.datePosted}</div>
     return (
       <div>
@@ -50,6 +57,7 @@ export class QuestionItem extends React.Component {
         rightAvatar = {<div className={classes.avatar}><Avatar size={25} color="#26A65B" backgroundColor="white">52</Avatar></div>}
         rightIconButton = {<div className={classes.buttonThumbsUp}><IconButton>
                            <ThumbsUp color="#26A65B" /></IconButton></div>}
+        onTouchTap={ touchQuestion }
       	/>
       </div>
     )
