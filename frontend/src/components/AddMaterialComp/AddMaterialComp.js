e950631ee798a384d1fd1c51a711d38d96f1e0a7
mistydevil <m.rohan.a.asmat@gmail.com>
2016-06-14 12:09:23 +0200

    material comp more flatter

--- frontend/src/components/AddMaterialComp/AddMaterialComp.js
+++ frontend/src/components/AddMaterialComp/AddMaterialComp.js
@@ -31,71 +31,9 @@ export class AddMaterialComp extends React.Component {
       margin: 8.5,
       backgroundColor: '#446CB3 ',
       color: '#ffffff',
-      borderTopRightRadius: 30,
-      borderBottomRightRadius:30,
       overflow: 'inherit',
       alignItems: 'center',
     }
-
-    const styleTwo = {
-      float: 'left',
-      height: 170,
-      width: 25,
-      margin: 8.5,
-      backgroundColor: '#ffffff',
-      color: '#ffffff',
-      borderTopRightRadius: 30,
-      borderBottomRightRadius:30,
-      overflow: 'inherit',
-      alignItems: 'center',
-      postion: 'absolute',
-      margin:'auto',
-      marginTop: 9,
-      marginLeft: -34,
-      borderStyle: 'solid',
-      borderWidth: 1,
-      borderColor: '#446CB3 ',
-    }
-
-    const styleThree = {
-      float: 'left',
-      height: 170,
-      width: 22,
-      margin: 8.5,
-      backgroundColor: '#446CB3 ',
-      color: '#ffffff',
-      borderTopRightRadius: 30,
-      borderBottomRightRadius:30,
-      overflow: 'inherit',
-      alignItems: 'center',
-      postion: 'absolute',
-      margin:'auto',
-      marginTop: 9,
-      marginLeft: -36,
-      borderStyle: 'solid',
-      borderWidth: 1,
-      borderColor: '#446CB3 ',
-    }
-
-    const styleFour = {
-      float: 'left',
-      height: 172,
-      width: 10,
-      margin: 8.5,
-      backgroundColor: '#000000',
-      color: '#ffffff',
-      overflow: 'inherit',
-      alignItems: 'center',
-      postion: 'absolute',
-      margin:'auto',
-      marginTop: 9,
-      marginLeft: -179,
-      borderStyle: 'solid',
-      borderWidth: 1,
-      borderColor: '#000000',
-      opacity: 0.7,
-    }
-
 
 	const plusButton ={
 	  postion: 'absolute',
@@ -125,9 +63,6 @@ export class AddMaterialComp extends React.Component {
         <div>
           <Paper style={style} zDepth={2}  children={nodePaperCourse}
             onTouchTap={this.props.show}/>
-          <Paper style={styleTwo} zDepth={0}  />
-          <Paper style={styleThree} zDepth={0}  />
-          <Paper style={styleFour} zDepth={5} />
         </div>
         {addMaterialModal}
       </div>
