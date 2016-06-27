2647f2caad68fd411861b37b33ca2420e4b448e0
mistydevil <m.rohan.a.asmat@gmail.com>
2016-06-27 11:11:59 +0200

    refactoring friendsDisplayComponent

--- frontend/src/components/FriendsDisplayComponent/FriendsDisplayComponent.js
+++ frontend/src/components/FriendsDisplayComponent/FriendsDisplayComponent.js
@@ -1,77 +1,58 @@
 import React, { PropTypes } from 'react'
-import Paper from 'material-ui/Paper';
-import Delete from 'material-ui/svg-icons/action/delete';
-import Friend from 'material-ui/svg-icons/social/person';
-import PageView from 'material-ui/svg-icons/action/pageview';
-import IconButton from 'material-ui/IconButton';
-import Divider from 'material-ui/Divider';
+import Paper from 'material-ui/Paper'
+import Friend from 'material-ui/svg-icons/social/person'
+import IconButton from 'material-ui/IconButton'
 
-type Props = {
+function FriendsDisplayComponent({ fullName, discipline, universityName }) {
+  const friendsPaperStyle = {
+    float: 'left',
+    height: 170,
+    width: 300,
+    margin: 8.5,
+    backgroundColor: 'white',
+    color: '#1690DB',
+    borderRadius: 13,
+    overflow: 'inherit',
+    alignItems: 'center',
+    borderStyle: 'solid',
+    borderWidth: 1,
+    borderColor: '#1690DB',
+  }
 
-	fullName: string,
-	discipline: string,
-	universityName: string,
-	avatarUrl: string
-};
-export class FriendsDisplayComponent extends React.Component {
+  const friendsButtonStyle = {
 
-  static propTypes = {
-  	fullName: PropTypes.string,
-  	discipline: PropTypes.string,
-  	universityName: PropTypes.string,
-  	avatarUrl: PropTypes.string
-  };
+    margin: 'auto',
+    width: '100%',
+    height: 50,
+    lineHeight: 50,
+  }
 
-  render () {
+  const noteFriendsStyle = {
+    width: 60,
+    height: 60,
+  }
 
-  	const friendsPaperStyle = {
-	  float: 'left',
-	  height: 170,
-	  width: 300,
-	  margin: 8.5,
-	  backgroundColor: 'white',
-	  color: '#1690DB',
-	  borderRadius: 13,
-	  overflow: 'inherit',
-	  alignItems: 'center',
-	  borderStyle: 'solid',
-	  borderWidth: 1,
-	  borderColor: '#1690DB'
-	};
+  const header = <div><h3>{fullName}</h3><h5>{universityName}</h5><h6>{discipline}</h6></div>
+  const container = <div><IconButton disableTouchRipple style={friendsButtonStyle}> <Friend style={noteFriendsStyle} /></IconButton></div>
 
-	const divActionStyles = {
-		float:'right'
-	};
+  const nodeFriendsComp = [
 
-	const friendsButtonStyle = {
+    header,
+    container,
+  ]
 
-	  margin:'auto',
-	  width:'100%',
-	  height: 50,
-	  lineHeight: 50
-	};
-
-	const noteFriendsStyle ={
-	  width: 60,
-	  height: 60
-	};
-
-	const header = <div><h3>{this.props.fullName}</h3><h5>{this.props.universityName}</h5><h6>{this.props.discipline}</h6></div>;
-	const container = <div><IconButton disableTouchRipple={true} style={friendsButtonStyle}> <Friend style={noteFriendsStyle} /></IconButton></div> ;
-
-	const nodeFriendsComp = [
-
-      	header,
-		container
-    ];
-
-    return (
+  return (
 
       <div>
-      	<Paper style={friendsPaperStyle} zDepth={1} children={nodeFriendsComp}  />
+        <Paper style={friendsPaperStyle} zDepth={1} children={nodeFriendsComp} />
       </div>
     )
-  }
+}
+
+FriendsDisplayComponent.propTypes = {
+  fullName: PropTypes.string,
+  discipline: PropTypes.string,
+  universityName: PropTypes.string,
 }
 
 export default FriendsDisplayComponent
