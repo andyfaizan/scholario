2029d922936ef7bc93f245b824e92389b521a1b7
mistydevil <m.rohan.a.asmat@gmail.com>
2016-06-27 12:54:27 +0200

    forgot pass refactor

---
+++ frontend/src/components/ForgotPassword/ForgotPassword.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { Card, CardHeader, CardText } from 'material-ui/Card'
+import Divider from 'material-ui/Divider'
+import LiveHelp from 'material-ui/svg-icons/communication/live-help'
+import ForgotPasswordForm from '../../forms/ForgotPasswordForm/ForgotPasswordForm'
+import classes from './ForgotPassword.scss'
+
+function ForgotPassword() {
+  let feedbackMessage
+  const feedbackTrue = this.props.feedbackTrue
+
+  if (feedbackTrue === 0) {
+    feedbackMessage = <div className={classes.error}>Falsche E-Mail-Konto</div>
+  } else if (feedbackTrue === 1) {
+    feedbackMessage = <div className={classes.success}>E-Mail wurde auf Ihr Konto gesendet</div>
+  } else {
+    feedbackMessage = ''
+  }
+
+  return (
+    <div className={classes.rootForgotPass}>
+      <div className={classes.forgotPassword}>
+        <div className={classes.inner} >
+          <Card>
+            <CardHeader
+              title="Passwort vergessen. Bitte geben Sie Ihre E-Mail Addresse"
+              titleStyle={classes.titleStyle}
+              titleColor="#26A65B"
+              avatar={<LiveHelp style={classes.iconStyle} />}
+            />
+            <Divider />
+            <CardText>
+              <div className={classes.containingEmail}>
+                <ForgotPasswordForm onSubmit={this.props.onSubmitForgotPassword} />
+              </div>
+              <br />
+              <div className={classes.feedback}>
+                <h4>{feedbackMessage}</h4>
+              </div>
+              <br />
+            </CardText>
+          </Card>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ForgotPassword
