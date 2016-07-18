e1460c901ade5bb3112aeb9f088dffd14c109cb7
mistydevil <m.rohan.a.asmat@gmail.com>
2016-07-18 16:58:21 +0200

    usr profile + chart progress dummy

---
+++ frontend/src/views/UserProfileView/UserProfileView.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+type Props = {
+
+};
+export class UserProfile extends React.Component {
+  props: Props;
+
+  render () {
+    return (
+      <div></div>
+    )
+  }
+}
+
+export default UserProfile
