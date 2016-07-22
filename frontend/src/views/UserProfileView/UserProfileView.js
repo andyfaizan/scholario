902a0cd5439c5ded54c6e0bc83a3a75080d57c59
mistydevil <m.rohan.a.asmat@gmail.com>
2016-07-22 11:57:21 +0200

    dumb

--- frontend/src/views/UserProfileView/UserProfileView.js
+++ frontend/src/views/UserProfileView/UserProfileView.js
@@ -3,12 +3,15 @@ import React from 'react'
 type Props = {
 
 };
+
 export class UserProfile extends React.Component {
   props: Props;
 
   render () {
     return (
-      <div></div>
+      <div>
+
+      </div>
     )
   }
 }
