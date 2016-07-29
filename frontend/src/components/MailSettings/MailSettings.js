1df5ec74d079f0795f91e0dd160eb7ee91068073
mistydevil <m.rohan.a.asmat@gmail.com>
2016-07-29 14:17:00 +0200

    settings View Dummy comps

---
+++ frontend/src/components/MailSettings/MailSettings.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+type Props = {
+
+};
+export class MailSettings extends React.Component {
+  props: Props;
+
+  render () {
+    return (
+      <div></div>
+    )
+  }
+}
+
+export default MailSettings
+
---
+++ frontend/src/components/PrivacySettings/PrivacySettings.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+type Props = {
+
+};
+export class PrivacySettings extends React.Component {
+  props: Props;
+
+  render () {
+    return (
+      <div></div>
+    )
+  }
+}
+
+export default PrivacySettings
+
