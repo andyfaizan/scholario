4cc5031ee8740966b9f1b60ee574aff8efaee44e
Ainuddin Faizan <andyfaizan@gmail.com>
2016-08-13 03:07:29 +0530

    Experimenting with bar graph

--- frontend/src/components/VerticalBarChart/VerticalBarChart.jsx
+++ frontend/src/components/VerticalBarChart/VerticalBarChart.jsx
@@ -11,27 +11,33 @@ const sampleData = {
   labels: ['Physics', 'Chemistry', 'Math', 'English', 'Deutsch'],
   datasets: [
     {
-      label: 'Course Progress',
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)',
-      ],
-      borderColor: [
-        'rgba(255,99,132,1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)',
-      ],
+      label: 'First Sem Progress',
+      backgroundColor: 'rgba(255, 99, 132, 0.2)',
+      // [
+      //   'rgba(255, 99, 132, 0.2)',
+      //   'rgba(54, 162, 235, 0.2)',
+      //   'rgba(255, 206, 86, 0.2)',
+      //   'rgba(75, 192, 192, 0.2)',
+      //   'rgba(153, 102, 255, 0.2)',
+      // ],
+      borderColor: 'rgba(255,99,132,1)',
+      // [
+      //   'rgba(255,99,132,1)',
+      //   'rgba(54, 162, 235, 1)',
+      //   'rgba(255, 206, 86, 1)',
+      //   'rgba(75, 192, 192, 1)',
+      //   'rgba(153, 102, 255, 1)',
+      // ],
       borderWidth: 1,
-      data: [65, 59, 80, 81, 56, 55, 40],
+      data: [65, 59, 80, 81, 56],
     },
   ],
+}
+
+const options = {
+  hover: {
+    mode: 'label',
+  },
 }
 
 function VerticalBarChart() {
@@ -40,7 +46,12 @@ function VerticalBarChart() {
   return (
     <div>
       <div style={styles.graphContainer}>
-        <BarChart data={sampleData} width="330" height="300" />
+        <BarChart
+          data={sampleData}
+          width="330"
+          height="300"
+          options={options}
+        />
       </div>
     </div>
   )
