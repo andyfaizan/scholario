feb14a8050e77740cb6ce272eed04ee141c3691f
Ainuddin Faizan <andyfaizan@gmail.com>
2016-08-13 02:27:15 +0530

    Created basic user profile card

--- frontend/src/components/UserDetailDisplay/UserDetailDisplay.jsx
+++ frontend/src/components/UserDetailDisplay/UserDetailDisplay.jsx
@@ -1,9 +1,16 @@
 import React, { PropTypes } from 'react'
 import Radium from 'radium'
 
+import Grid from 'react-bootstrap/lib/Grid'
+import Row from 'react-bootstrap/lib/Row'
+import Col from 'react-bootstrap/lib/Col'
+
 import Avatar from 'material-ui/Avatar'
-import ListItem from 'material-ui/List/ListItem'
+import FlatButton from 'material-ui/FlatButton'
+import PersonAdd from 'material-ui/svg-icons/social/person-add'
 import Divider from 'material-ui/Divider'
+
+import Christoph from './christoph.png'
 
 import ActivityOfUser from '../../components/ActivityOfUser/ActivityOfUser'
 
@@ -11,31 +18,69 @@ const propTypes = {
   nameInitialForAvatar: PropTypes.string,
   fullName: PropTypes.string,
   universityName: PropTypes.string,
-  programmeEnrolled: PropTypes.stirng,
+  programEnrolled: PropTypes.stirng,
   socialConnects: PropTypes.array,
 }
 
-function UserDetailDisplay() {
+const defaultProps = {
+  nameInitialForAvatar: 'M',
+  fullName: 'Christoph Johannes Walpert',
+  universityName: 'RWTH Aachen',
+  programEnrolled: 'Medieninformatik',
+  socialConnects: ['Lars', 'Rohan'],
+}
+
+function UserDetailDisplay({ fullName, universityName, programEnrolled, socialConnects }) {
   const styles = getStyles()
+  const uniField = 'Hochschule'
+  const programField = 'Program'
+  const connectsField = 'Connects'
 
   return (
     <div>
-      <ListItem
-        leftAvatar={
-          <Avatar>
-            A
-          </Avatar>
-        }
-      >
-        Letter Avatar  custom colors and size
-      </ListItem>
-      <div style={styles.userMetadata}>
-        <h5>RWTH AAachen</h5>
-        <h5>Media  Informatics</h5>
-        <h5>Social Connects </h5>
-        <br />
-      </div>
-      <Divider />
+      <Grid className="container-fluid">
+        <Row>
+          <Col xs={3} md={2}>
+            <Avatar src={Christoph} size={200} />
+          </Col>
+          <Col xs={5} md={4}>
+            <div style={styles.userName}>
+              <h2>{fullName}</h2>
+            </div>
+            <Col xs={4}>
+              <div style={styles.userMetadata}>
+                <br />
+                <h5><strong>{uniField}</strong></h5>
+                <h5><strong>{programField}</strong></h5>
+                <h5><strong>{connectsField}</strong></h5>
+                <br />
+              </div>
+            </Col>
+            <Col xs={8}>
+              <div style={styles.userMetadata}>
+                <br />
+                <h5>{universityName}</h5>
+                <h5>{programEnrolled}</h5>
+                {socialConnects.map((c) =>
+                  <Avatar
+                    style={styles.connectStyle}
+                  >
+                    {c.charAt(0)}
+                  </Avatar>)}
+                <br />
+              </div>
+            </Col>
+          </Col>
+          <Col xs={1} md={1}>
+            <FlatButton
+              primary={false}
+              icon={<PersonAdd />}
+            />
+          </Col>
+        </Row>
+      </Grid>
+      <br />
+      <br />
       <br />
       <ActivityOfUser />
     </div>
@@ -50,13 +95,20 @@ function getStyles() {
     },
     userMetadata: {
       position: 'relative',
-      marginLeft: '75px',
+      marginLeft: '35px',
       marginTop: '-10px',
+    },
+    userName: {
+      position: 'relative',
+      marginLeft: '45px',
+    },
+    connectStyle: {
+      marginRight: '5px',
     },
   }
 }
 
 UserDetailDisplay.propTypes = propTypes
+UserDetailDisplay.defaultProps = defaultProps
 
 export default Radium(UserDetailDisplay)
-
